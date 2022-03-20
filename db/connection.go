e940d445efd234554d6673b4c7166b030733efaf
Rajnish Singh <10815566+rajnish93@users.noreply.github.com>
2022-03-20 22:25:49 +0530

    Added DB Connection

---
+++ db/connection.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connection() *gorm.DB {
+	var (
+		host     = os.Getenv("POSTGRES_HOST")
+		user     = os.Getenv("POSTGRES_USER")
+		port     = os.Getenv("POSTGRES_PORT")
+		password = os.Getenv("POSTGRES_PASSWORD")
+		name     = os.Getenv("POSTGRES_DB")
+	)
+	connectionstr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, name)
+	DB, err := gorm.Open(postgres.Open(connectionstr), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+
+	if err != nil {
+		log.Fatalf("Error in connect the DB %v", err)
+		return nil
+	}
+
+	if DB.Error != nil {
+		log.Fatalln("Any Error in connect the DB " + err.Error())
+		return nil
+	}
+	log.Print("Connection to DB: Success")
+
+	return DB
+}
